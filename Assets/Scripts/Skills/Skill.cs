a1c97cef90397281c5639c8a51fe28cf2135e152
Bryan Tai <redhatpieman@gmail.com>
2020-02-01 12:37:34 -0800

    Add skill cooldown refreshed event

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public abstract class Skill : MonoBehaviour
     [SerializeField] protected SkillData SkillData;
     [HideInInspector] protected float RemainingCoolDown;
     public abstract SkillType SkillType { get; }
+    public event Action OnCoolDownRefreshed;
 
     public abstract void CastSkill(List<TeamMate> teamMates, int targetIndex);
     public abstract bool CanCast(List<TeamMate> teamMates, int targetIndex);
@@ -16,5 +18,10 @@ public abstract class Skill : MonoBehaviour
         if (RemainingCoolDown == 0) return;
 
         RemainingCoolDown = Mathf.Max(RemainingCoolDown - Time.deltaTime, 0);
+
+        if (RemainingCoolDown == 0)
+        {
+            OnCoolDownRefreshed();
+        }
     }
 }
