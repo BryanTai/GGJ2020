5f4e89c2702910010b2c176570f6c63188d0dbcb
Bryan Tai <redhatpieman@gmail.com>
2020-02-01 14:01:57 -0800

    Added support for switching teammate faces

---
+++ Assets/Scripts/UI/TeammateFaces.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TeammateFaces", order = 1)]
+public class TeammateFaces : ScriptableObject
+{
+    [SerializeField]
+    public enum FaceType
+    {
+        HAPPY,
+        NEUTRAL,
+        WORSE,
+        BAD,
+        ATTACKING
+    }
+
+    public Sprite[] Faces;
+
+    public Sprite GetFaceForFaceType(FaceType type)
+    {
+        return Faces[(int)type];
+    }
+}
