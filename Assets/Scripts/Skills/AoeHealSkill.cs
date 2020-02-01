a1fb3a3b5879b9f2303705fc9f6aaa523d360b87
beamer1989 <beamer1989@gmail.com>
2020-02-01 12:10:20 -0800

    basic canCast function

--- Assets/Scripts/Skills/AoeHealSkill.cs
+++ Assets/Scripts/Skills/AoeHealSkill.cs
@@ -20,6 +20,13 @@ public class AoeHealSkill : Skill
 
     public override bool CanCast(List<TeamMate> teamMates, int targetIndex)
     {
-        return true;
+        if(teamMates[targetIndex].Health != 0 && this.RemainingCoolDown == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
