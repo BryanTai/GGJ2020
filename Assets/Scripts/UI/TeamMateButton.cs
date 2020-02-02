d15a6c717ef2432b3e00c5b66dea2581a538afdb
Bryan Tai <redhatpieman@gmail.com>
2020-02-01 22:18:37 -0800

    Added Mood icons to match changing moods

--- Assets/Scripts/UI/TeamMateButton.cs
+++ Assets/Scripts/UI/TeamMateButton.cs
@@ -9,14 +9,28 @@ public class TeamMateButton : UIButton
     public Slider healthBar;
     public TeammateFaces faceReferences;
     public TeamMateClass teamMateClass;
-    public TeamMate teamMate;
+    [HideInInspector] public TeamMate teamMate;
+
+    public Image moodIconImage;
+    public List<Sprite> moodIcons;
+
+    private int happyMoodThreshold;
+    private int neutralMoodThreshold;
+
+    public Text debugMoodText; //TODO REMOVE THIS
 
     public override void InitButton(int index)
     {
         SetHealth(teamMate.MaxHealth);
+        SetMoodIconFromMoodValue(teamMate.BiggestMood);
+
         teamMateClass = (TeamMateClass) index;
-        SetFaceFromMood(TeamMateMood.NEUTRAL);
+        SetButtonImageFromMood(TeamMateMood.NEUTRAL);
         teamMate.OnHealthChanged += SetHealth;
+        teamMate.OnMoodChanged += SetMoodIconFromMoodValue;
+
+        happyMoodThreshold = (int) (teamMate.BiggestMood * 0.66f);
+        neutralMoodThreshold = (int) (teamMate.BiggestMood * 0.33f);
     }
 
     public void SetHealth(int newHealth)
@@ -24,10 +38,28 @@ public class TeamMateButton : UIButton
         healthBar.value = newHealth / (float) teamMate.MaxHealth;
     }
 
-    public void SetFaceFromMood(TeamMateMood mood)
+    public void SetButtonImageFromMood(TeamMateMood mood)
     {
         uiButton.image.sprite = faceReferences.GetFaceForMood(mood);
     }
 
-    
+    public void SetMoodIconFromMoodValue(int mood)
+    {
+        int maxMood = teamMate.BiggestMood;
+
+        debugMoodText.text = mood.ToString();
+
+        if(mood > happyMoodThreshold)
+        {
+            moodIconImage.sprite = moodIcons[0];
+        }
+        else if(mood > neutralMoodThreshold)
+        {
+            moodIconImage.sprite = moodIcons[1];
+        }
+        else
+        {
+            moodIconImage.sprite = moodIcons[2];
+        }
+    }
 }
